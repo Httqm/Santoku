f2f95b57df12ca2e06f686a6e55ae6ef473bf34d
Matthieu Fournet <fournet.matthieu@gmail.com>
2012-05-05 11:28:42 +0200

    Added AGPL Copyright notice

--- modules/services.py
+++ modules/services.py
@@ -1,4 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# Copyright (C) 2012 Matthieu FOURNET, fournet.matthieu@gmail.com
+#
+# This file is part of Santoku.
+#
+# Santoku is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Santoku is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Santoku.  If not, see <http://www.gnu.org/licenses/>
+#
+
 
 from modules import config as c
 
