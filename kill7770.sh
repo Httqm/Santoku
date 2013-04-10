f03bfd404a38a5e93744055b5d452896d30b4b81
Matthieu Fournet <fournet.matthieu@gmail.com>
2013-04-10 11:45:50 +0200

    Cosmetic

--- kill7770.sh
+++ kill7770.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ######################################### kill7770.sh ###############################################
-# Sometimes, the Python process listening on port 7770 doesn't stop when required to do so.
+# Sometimes, the Python process listening on port 7770 doesn't stop when required to do so. (WHY ???)
 # Thus, Shinken can not restart since there's already someone listening on port 7770.
 # 
-# This script is to automate the killing of this process
+# This script is to automate the killing of this process.
 ######################################### ###########################################################
 
-python7770Pid=$(netstat -laputen | grep ":7770" | awk '{ print $9}' | cut -d '/' -f 1);
-#echo $python7770Pid;
-[ "$python7770Pid" != "" -a -d "/proc/$python7770Pid" ] && {
-	echo -n "Killing PID $python7770Pid ... "
-	kill -1 $python7770Pid && echo 'OK' || echo 'KO';
+pidOfPythonProcessListeningOnPort7770=$(netstat -laputen | grep ":7770" | awk '{ print $9}' | cut -d '/' -f 1);
+
+[ "$pidOfPythonProcessListeningOnPort7770" != "" -a -d "/proc/$pidOfPythonProcessListeningOnPort7770" ] && {
+	echo -n "Killing PID $pidOfPythonProcessListeningOnPort7770 ... "
+	kill -1 $pidOfPythonProcessListeningOnPort7770 && echo 'OK' || echo 'KO';
 	} || echo 'No PID to kill.'
